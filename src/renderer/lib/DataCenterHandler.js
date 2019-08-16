17930a1ad23aa4554bf7b709623587bb3af2adde
D067430 <minhao.fei01@sap.com>
2019-08-16 17:31:03 +0200

    add data center handler

---
+++ src/renderer/lib/DataCenterHandler.js
@@ -0,0 +1,28 @@
+const request = require('request')
+const dataCenterUrl = "http://localhost:3000/mining-pool/"
+
+const DataCenterHandler = {
+	createWallet: function (params, callback) {
+		request({
+			url: `${dataCenterUrl}createwallet`,
+			method: 'POST',
+			body: params,
+			json: true
+		}, (err, res, body) => {
+			callback(body)
+		})
+	},
+
+	findOutWallet: function (params, callback) {
+		request({
+			url: `${dataCenterUrl}finditem`,
+			method: 'POST',
+			body: params,
+			json: true
+		}, (err, res, body) => {
+			callback(body)
+		})
+	}
+}
+
+module.exports = DataCenterHandler
