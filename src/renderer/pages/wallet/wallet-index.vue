9daca715cf8f6b1e407b240b1bf1a60656109f36
Yuan Li <abysmli@gmail.com>
2019-09-10 11:36:19 +0200

    change getBalance frequence back

--- src/renderer/pages/wallet/wallet-index.vue
+++ src/renderer/pages/wallet/wallet-index.vue
@@ -534,7 +534,7 @@ export default {
         console.log('Job ID')
         this._getWalletBalance(this.selectedWallet.walletAddress)
         this._getWalletTransactions(this.selectedWallet.walletAddress)
-      }, 3 * 60 * 1000)
+      }, 30000)
     },
 
     _getWalletBalance(walletAddress) {
